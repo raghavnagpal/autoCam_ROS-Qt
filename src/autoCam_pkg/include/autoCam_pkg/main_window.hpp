977210b8976737298c093bf47cff63f9e0bcb581
swhiteinventor <swhiteinventor@gmail.com>
2020-04-22 14:17:38 -0400

    removed logs

--- src/autoCam_pkg/include/autoCam_pkg/main_window.hpp
+++ src/autoCam_pkg/include/autoCam_pkg/main_window.hpp
@@ -32,11 +32,11 @@ class MainWindow : public QMainWindow {
 Q_OBJECT
 
 public:
-	MainWindow(int argc, char** argv, QWidget *parent = 0);
-	~MainWindow();
+  MainWindow(int argc, char** argv, QWidget *parent = 0);
+  ~MainWindow();
 
-	void ReadSettings(); // Load up qt program settings at startup
-	void WriteSettings(); // Save qt program settings when closing
+//	void ReadSettings(); // Load up qt program settings at startup
+//	void WriteSettings(); // Save qt program settings when closing
 
 	void closeEvent(QCloseEvent *event); // Overloaded function
 	void showNoMasterMessage();
@@ -45,14 +45,14 @@ public Q_SLOTS:
 	/******************************************
 	** Auto-connections (connectSlotsByName())
 	*******************************************/
-	void on_actionAbout_triggered();
-	void on_button_connect_clicked(bool check );
-	void on_checkbox_use_environment_stateChanged(int state);
+//	void on_actionAbout_triggered();
+//	void on_button_connect_clicked(bool check );
+//	void on_checkbox_use_environment_stateChanged(int state);
 
     /******************************************
     ** Manual connections
     *******************************************/
-    void updateLoggingView(); // no idea why this can't connect automatically
+//    void updateLoggingView(); // no idea why this can't connect automatically
 
 private:
 	Ui::MainWindowDesign ui;
